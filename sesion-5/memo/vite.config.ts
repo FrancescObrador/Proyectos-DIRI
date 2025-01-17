aa098503d829f874c29ef810f4789aaf2533e30d
Francesc Obrador <obrador.francesc@gmail.com>
2025-01-17 16:11:47 +0100

    build de memo

--- sesion-5/memo/vite.config.ts
+++ sesion-5/memo/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Proyectos-DIRI/sesion-5/memo/', 
+  build: {
+    outDir: 'docs', // Para Github Pages
+  }
 })
